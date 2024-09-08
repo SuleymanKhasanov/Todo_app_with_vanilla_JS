f8b86cf35582364921ed18077247a4aff59dcb87
Сулейман Хасанов <macbook@MacBook-Air.local>
2024-09-08 16:12:02 +0500

    Added task list v1.1.0

--- script/main.js
+++ script/main.js
@@ -3,6 +3,7 @@ import { todos } from './data';
 const input = document.getElementById('input');
 const button = document.getElementById('button');
 const form = document.getElementById('form');
+const list = document.getElementById('todo-list');
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
@@ -11,4 +12,15 @@ form.addEventListener('submit', (e) => {
 button.addEventListener('click', () => {
   todos.push(input.value);
   input.value = '';
+
+  new Promise((resolve, reject) => {
+    if (todos) {
+      const newListItem = document.createElement('li');
+      newListItem.classList.add('todo-list__item');
+      newListItem.textContent = todos[todos.length - 1];
+      resolve(list.appendChild(newListItem));
+    } else {
+      reject(null);
+    }
+  });
 });
