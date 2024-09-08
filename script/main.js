cf4ce7403c59b74904ba67f69dd21300f27170b7
Сулейман Хасанов <macbook@MacBook-Air.local>
2024-09-08 17:58:28 +0500

    Added done pins v1.2.0

--- script/main.js
+++ script/main.js
@@ -4,11 +4,14 @@ const input = document.getElementById('input');
 const button = document.getElementById('button');
 const form = document.getElementById('form');
 const list = document.getElementById('todo-list');
+const totalCounter = document.getElementById('total-counter');
+const doneCounter = document.getElementById('done-task-counter');
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
 });
 
+totalCounter.textContent = 0;
 button.addEventListener('click', () => {
   todos.push(input.value);
   input.value = '';
@@ -19,8 +22,28 @@ button.addEventListener('click', () => {
       newListItem.classList.add('todo-list__item');
       newListItem.textContent = todos[todos.length - 1];
       resolve(list.appendChild(newListItem));
+      const checkbox = document.createElement('input');
+      checkbox.type = 'checkbox';
+      checkbox.classList.add('checkbox');
+      newListItem.appendChild(checkbox);
+
+      checkbox.addEventListener('change', () => {
+        if (checkbox.checked) {
+          newListItem.classList.add('inactive');
+        } else {
+          newListItem.classList.remove('inactive');
+        }
+        function updateCheckedCounter() {
+          const checkedCheckboxes = document.querySelectorAll(
+            '.checkbox:checked',
+          );
+          doneCounter.textContent = checkedCheckboxes.length;
+        }
+        updateCheckedCounter();
+      });
     } else {
       reject(null);
     }
   });
+  totalCounter.textContent = todos.length;
 });
